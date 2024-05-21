c4f6115d92b7d3a473ecc118117e32097f058dd7
rellyrelly <thedudesaysdude@gmail.com>
2024-05-21 15:53:07 -0400

    player movement YIPPEE

--- assets/code/basePlayer.gd
+++ assets/code/basePlayer.gd
@@ -9,10 +9,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction: Vector2 = Input.get_vector("Left","Right","Up","Down")
+	var input_x = Input.get_axis("Left","Right")
+	var input_z = Input.get_axis("Up","Down")
+	var direction = (self.transform.basis * Vector3(input_x,0,input_z)).normalized()
 	
-	velocity.x = move_toward(velocity.x, direction.x*base_speed, base_speed)
-	velocity.y = move_toward(velocity.y, direction.y*base_speed, base_speed)
+	velocity.x = move_toward(velocity.x, direction.x, base_speed)
+	velocity.z = move_toward(velocity.y, direction.z, base_speed)
 	
 	
 	move_and_slide()
