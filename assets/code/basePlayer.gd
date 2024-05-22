35302ebb943cb1560a85415529a03ec64ea03391
rellyrelly <thedudesaysdude@gmail.com>
2024-05-22 18:39:03 -0400

    uhh idk small changes

--- assets/code/basePlayer.gd
+++ assets/code/basePlayer.gd
@@ -2,10 +2,18 @@ class_name PlayerEntity
 
 extends BaseEntity
 
+<<<<<<< Updated upstream
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+=======
+@onready var area_3d = $Area3D
+@onready var animation_tree = $AnimationTree
+@onready var animation_player = $AnimationPlayer
+>>>>>>> Stashed changes
+
+var current_pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,3 +26,20 @@ func _process(delta):
 	
 	
 	move_and_slide()
+<<<<<<< Updated upstream
+=======
+
+func input():
+	var inputdir = Input.get_vector("Left","Right", "Up", "Down")
+	if inputdir:
+		direction.x = move_toward(direction.x, inputdir.x, 0.1)
+		direction.y = move_toward(direction.y, inputdir.y, 0.1)
+
+	
+	area_3d.look_at($".".global_transform.origin)
+	
+	velocity.x = move_toward(velocity.x, inputdir.x, base_speed)
+	velocity.z = move_toward(velocity.z, inputdir.y, base_speed)
+	
+	
+>>>>>>> Stashed changes
