05fcce4d9f684a6bba628d51d730178c2ead3170
ayechp <mockthirteen@gmail.com>
2024-05-24 17:23:15 -0400

    GlobalScript
    Enums/Area check base

---
+++ assets/code/GlobalScript.gd
@@ -0,0 +1,20 @@
+extends Node
+
+enum Area{First, Second, Third}
+var CurrentArea
+
+func areaSet():
+	## or func areaSet(Area x)
+	##area boss flag condiction or just call when defeating a boss/getting an item
+	##CurrentArea = area.First / x
+	pass
+
+##func areaGet():
+	##return CurrentArea
+	##not really needed since currentArea is global
+##	pass
+	
+func _process(delta):
+	##check CurrentArea and change map/tileset accordingly
+	##tried checking out changing the tilesets via function but ran into a block when it came to non homogenous tilesets
+	pass
