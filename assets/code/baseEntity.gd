ca5b5f5175418210c4605d5992826671fd168031
Andromeda <109443978+Andrymeda@users.noreply.github.com>
2024-05-31 16:52:55 -0300

    I KEEP FORGETTING TO COMMIT AS I WORk, OOPS.

--- assets/code/baseEntity.gd
+++ assets/code/baseEntity.gd
@@ -8,9 +8,12 @@ var max_health: int = 100
 var direction := Vector2(0, 1)
 var gravity := 10
 
+@export var control := true # Wether or not the creature can move
+@export var lockdir := false # Wether or not to lock the direction of the creature
 @export var current_health: int = 100
 @export var base_speed: float = 3.0 # Might want to make an int to be consistent but float might be better for calcs
 @export var base_damage: int = 10 
+@export var friction := 0.8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,6 +34,7 @@ func attack():
 	# This function will likely be empty at this level because each enemy attacks differently
 	pass
 	
+	
 func set_health(health):
 	current_health = clamp(health, 0, 100)
 
@@ -47,3 +51,9 @@ func on_hit(damage):
 func load_sprite():
 	pass
 
+
+func applyvelocity(givenVelocity : Vector3, towardsDir : bool):
+	if towardsDir:
+		velocity += givenVelocity.rotated(Vector3(0, 1, 0).normalized(), -direction.angle())
+	else:
+		velocity += givenVelocity
