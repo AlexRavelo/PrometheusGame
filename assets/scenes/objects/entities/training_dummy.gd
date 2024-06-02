fcce48f2eb2e393613428f4d327e7137bc71cd5a
Wallemations <66052067+Wallemations@users.noreply.github.com>
2024-06-01 20:23:46 -0400

    makes the training dummy work

--- assets/scenes/objects/entities/training_dummy.gd
+++ assets/scenes/objects/entities/training_dummy.gd
@@ -4,5 +4,20 @@ class_name Training_Dummy
 
 @onready var damage_label = $Label3D
 
+var current_dps = 0
+var DamageNum = preload("res://assets/scenes/test_scenes/damagenumbers.tscn")
+
 func on_hit(damage):
-	damage_label.text = str(damage)
+	current_dps += damage
+	damage_label.text = str(current_dps)
+	var attack = DamageNum.instantiate()
+	add_child(attack)
+	attack.play_float(damage)
+	await get_tree().create_timer(1).timeout
+	attack.queue_free()
+
+
+
+func _on_dps_reset_timeout():
+	current_dps = 0
+	damage_label.text = "0"
