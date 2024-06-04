e43f773a77fd2db104feb904ad98503e1d5dd11b
ayechp <mockthirteen@gmail.com>
2024-06-04 18:47:20 -0400

    Spawner
    we did it, we trimmed the fuck outta this

--- assets/code/GlobalScript.gd
+++ assets/code/GlobalScript.gd
@@ -2,22 +2,11 @@ extends Node
 
 enum Area{First, Second, Third}
 enum EnemyState{Idle, Alert, Attack, Retreat}
-enum EnemyType{Boss, Normal}
+enum EnemyType{Beaset,Axe, Spear}
 enum AttackProperty{Basic, Fire} # Will Add more
 var LitCampfires: Dictionary
 var CurrentArea
 
-func areaSet():
-	## or func areaSet(Area x)
-	##area boss flag condiction or just call when defeating a boss/getting an item
-	##CurrentArea = area.First / x
-	pass
-
-func areaGet():
-	##return CurrentArea
-	##not really needed since currentArea is global
-	return Area.First
-	
 func _process(_delta):
 	##check CurrentArea and change map/tileset accordingly
 	##tried checking out changing the tilesets via function but ran into a block when it came to non homogenous tilesets
--- assets/code/Spawner.gd
+++ assets/code/Spawner.gd
@@ -4,7 +4,6 @@ extends Node3D
 @export var type : GlobalScript.EnemyType
 var enemyDetails = []
 var enemyAxe = preload("res://assets/code/Enemies/axe_enemy.tscn")
-#var locationID = GlobalScript.CurrentArea TODO: Uncomment and delete below when we start using the setArea() function
 var locationID = GlobalScript.Area.First
 var enemy
 
@@ -25,19 +24,6 @@ func spawnEnemy(type):
 			pass
 
 
-#TODO: NEED HELP WITH THIS, VisibleOnScreenNotifier3D doesn't seem to work on afaik. Was thinking about binding it to the camera areas 
-#func OnCameraExit():
-	##destroy enemy object and store stats
-	#enemyDetails = []
-		#enemy.queue_free()
-	#else:
-		#print("Enemy Defeated")
-		#
-#func OnCameraReEnter():
-	#spawnEnemy()
-	#
-	#pass
-	
 func _ready():
 	spawnEnemy(type)
 	
