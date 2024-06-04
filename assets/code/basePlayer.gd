47288508686ada0dec4be99b3814e99f1797471a
rellyrelly <thedudesaysdude@gmail.com>
2024-06-04 12:40:20 -0400

    its here.... FireAttack
    when you forget to commit your changes

--- assets/code/basePlayer.gd
+++ assets/code/basePlayer.gd
@@ -14,12 +14,19 @@ extends BaseEntity
 @export var ClawAttackCooldown: float = 0.25
 
 var anim_state
+var attack_selection : AttackSelect
 var Attack1 = preload("res://assets/scenes/Attacks/Attack1.tscn")
+var Attack2 = preload("res://assets/scenes/Attacks/Attack2.tscn")
 
 enum States{
 	NEUTRAL,
 	ATTACKING,
 	ROLLING,
+}
+
+enum AttackSelect{
+	Claw = 2,
+	Fire,
 }
 
 func _ready():
@@ -56,7 +63,11 @@ func state_neutral(): # Neutral State: Idle, Running, ETC
 	var inputdir = Vector2(0, 0)
 	if control: 
 		inputdir = Input.get_vector("Left","Right", "Up", "Down")
-		if Input.is_action_just_pressed("Attack"):
+		if Input.is_action_just_pressed("ClawAttack"):
+			attack_selection = AttackSelect.Claw
+			state = States.ATTACKING
+		if Input.is_action_just_pressed("FireAttack"):
+			attack_selection = AttackSelect.Fire
 			state = States.ATTACKING
 	else:
 		inputdir = Vector2(0, 0)
@@ -84,7 +95,13 @@ func state_attacking(): #Attacking State: Regular Attack Handling.
 	if cooldown.time_left <= 0:
 		velocity = Vector3.ZERO
 		cooldown.start(ClawAttackCooldown)
-		anim_state.travel("AttackFront")
+		
+		match attack_selection:
+			AttackSelect.Claw:
+				anim_state.travel("AttackFront")
+			AttackSelect.Fire:
+				anim_state.travel("FireAttack")
+			
 		print("Ratchet ATTACK")
 	else:
 		state = States.NEUTRAL
@@ -103,6 +120,15 @@ func claw_attack():
 	await get_tree().create_timer(0.2).timeout
 	attack.queue_free()
 	#individual attack properties are located in their respective .gd scripts
+	
+func fire_attack():
+	var attack = Attack2.instantiate()
+	add_child(attack)
+	attack.damage = base_damage
+	attack.knockback = Vector3(0,0,0) # TODO CHANGE LATER
+	attack.look_at(Vector3(position.x + direction.x, position.y, position.z + direction.y))
+	await get_tree().create_timer(0.5).timeout
+	attack.queue_free()
 
 func on_death():
 	print("FUCK IM DEAD")
