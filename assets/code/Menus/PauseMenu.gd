868467c00102ddcfc90ed5d9ff8e658c17aa727e
rellyrelly <thedudesaysdude@gmail.com>
2024-06-09 15:34:20 -0400

    every button is named button !!!

--- assets/code/Menus/PauseMenu.gd
+++ assets/code/Menus/PauseMenu.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+func _ready():
+	pass
 
-func _on_resume_button_up():
+func _on_resume_button_button_up():
 	get_tree().paused = false
 	self.queue_free()
+
+
+func _on_options_button_button_up():
+	$Options.show()
