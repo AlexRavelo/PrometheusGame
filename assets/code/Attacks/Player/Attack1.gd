41f3e280a0b7221f3c2016248bd68a76c48ed2e2
rellyrelly <thedudesaysdude@gmail.com>
2024-06-13 16:00:37 -0400

    reimplements refactor after merge
    ITS FUCKING WORKING NOW ?? I CAN HIDE ATTACK1 IN EDITOR??

--- assets/code/Attacks/Player/Attack1.gd
+++ assets/code/Attacks/Player/Attack1.gd
@@ -1,10 +1,6 @@
 extends BaseAttack
 #claw attack
 
-func _ready():
-	set_visible(false) #this exists because you cannot hide the hitbox in the editor
-	#go ahead try it and run the game
-
 func do_attack(body):
 	if body.is_in_group("enemies"):
 		body.on_hit(self)
