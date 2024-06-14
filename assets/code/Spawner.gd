acb14d56b2d90512d5220832d30aaeb727449dbb
Nash Roy <nashroy11@gmail.com>
2024-06-14 01:30:00 -0400

    spawning in room system

--- assets/code/Spawner.gd
+++ assets/code/Spawner.gd
@@ -1,33 +1,46 @@
 class_name Spawner
 extends Node3D
-
+@export var spawn_enemy : bool = true
 @export var type : GlobalScript.EnemyType
+
 var enemyDetails = []
-var enemyAxe = preload("res://assets/code/Enemies/axe_enemy.tscn")
+#var enemyAxe = preload("res://assets/code/Enemies/axe_enemy.tscn")
 var enemyBeast= preload("res://assets/scenes/objects/entities/beast_enemy.tscn")
 var locationID = GlobalScript.Area.First
 var enemy
 
+func listener_entry():
+	spawnEnemy(GlobalScript.EnemyType.Beast)
+
+func listener_exit():
+	var enemy_list = get_children()
+	for n in enemy_list:
+		n.queue_free()
+
+
 func spawnEnemy(type):
-	match type: 
-		0:
-			enemy = enemyBeast.instantiate()
-			enemy.global_position = Vector3.ZERO
-			add_child(enemy)
+	print("AHHH")
+	if spawn_enemy == true:
+		match type: 
+			0:
+				enemy = enemyBeast.instantiate()
+				enemy.global_position = Vector3.ZERO
+				add_child(enemy)
+				
+			1:
+				pass
 			
-		1:
-			pass
-		
-		2:
-			pass
+			2:
+				pass
 
 
 func _ready():
-	spawnEnemy(type)
-	
+	#removed, as this will be triggered via signal now.
+	#spawnEnemy(type)
+	get_parent().connect("on_player_entry", listener_entry)
+	get_parent().connect("on_player_exit", listener_exit)
 
 func _process(delta):
-	
 	pass
 	
 
--- assets/scenes/Rooms/Room_Script.gd
+++ assets/scenes/Rooms/Room_Script.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
 @export var areaName: String
+@onready var spawner = $Spawner
+
+signal on_player_entry
+signal on_player_exit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	#spawner.on_player_entry.connect(spawner.Mama())
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,11 +17,10 @@ func _process(delta):
 
 
 func _on_area_3d_body_entered(body):
-	if(body is PlayerEntity):
-		print("entered area")
-
+	if body is PlayerEntity:
+		on_player_entry.emit()
 
 
 func _on_area_3d_body_exited(body):
-	if(body is PlayerEntity):
-		print("clean up area")
+	if body is PlayerEntity:
+		on_player_exit.emit()
